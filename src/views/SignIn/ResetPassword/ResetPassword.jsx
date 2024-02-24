5f446f37240b562155323d23273ac99757f78cb2
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-02-24 11:57:09 -0800

    update

--- src/views/SignIn/ResetPassword/ResetPassword.jsx
+++ src/views/SignIn/ResetPassword/ResetPassword.jsx
@@ -31,7 +31,7 @@ const SignUp = () => {
     const location = useLocation();
     const parts = location.pathname.split("/")
     const token = parts[parts.length - 1];
-    const userID = parseInt(parts[parts.length - 2]);
+    const userID = parts[parts.length - 2];
     console.log(parts);
     console.log(token);
     console.log(userID);
