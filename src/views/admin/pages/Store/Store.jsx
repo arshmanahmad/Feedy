94ab10618fde2bcd5edfdb07e3329eef7a9dade1
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-03-05 08:40:42 -0800

    update

--- src/views/admin/pages/Store/Store.jsx
+++ src/views/admin/pages/Store/Store.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WrapperCard from '../../../../components/CardLayout/WrapperCard/WrapperCard'
-
+import Table from './components/Table/Table'
 const Store = () => {
+    const [data, setData] = useState([
+        { name: "Arshman", skill: "React-developer" },
+        { name: "Shuja", skill: "React-developer" },
+        { name: "Ali", skill: "React-developer" },
+    ])
     return (
         <>
             <WrapperCard>
@@ -9,7 +14,7 @@ const Store = () => {
 
                 </div>
                 <div className="Store_table">
-
+                    <Table array={data} />
                 </div>
             </WrapperCard>
         </>
---
+++ src/views/admin/pages/Store/components/Table/Table.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Table = (array) => {
+    return (
+        <>
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Table
