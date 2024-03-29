8b8fe1dde1d3beade22d5dfe168dceb1cb0b7a05
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-03-29 13:35:46 -0700

    update

--- src/views/admin/pages/Order/components/OrdersTable/OrderTable.jsx
+++ src/views/admin/pages/Order/components/OrdersTable/OrderTable.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import './OrderTable.css'
-const OrderTable = ({ tableHeads, array, keysToDisplay }) => {
+const OrderTable = ({ tableHeads, array, keysToDisplay, modifiedColumn, ConditionalModifiedColumn }) => {
     return (
         <>
-            <table className='order_table'>
-                <thead>{tableHeads.map((item) => {
-                    return <th>{item}</th>
-                })}</thead>
-                <tbody>{array.map((items) => {
 
-                    return <tr>{
-                        keysToDisplay.map((dataItems) => {
-                            return <td>{items[dataItems]}</td>
-                        })
-                    }
-                    </tr>
+            <div className="store_tableContainer">
+                <table className='order_table'>
+                    <thead className='order_thead'>{tableHeads.map((item) => {
+                        return <th className='order_th'>{item}</th>
+                    })}</thead>
+                    <tbody>{array.map((items) => {
 
-                })}</tbody>
-            </table>
+                        return <tr>{
+                            keysToDisplay.map((dataItems) => {
+                                const conditions = ConditionalModifiedColumn || {};
+                                const content = conditions[dataItems] ? conditions[dataItems](items[dataItems]) : items[dataItems]
+                                return <td className='order_data'>{content}</td>
+                            })
+                        }
+                        </tr>
+
+                    })}</tbody>
+                </table>
+            </div>
         </>
     )
 }
--- src/views/admin/pages/Order/screens/Home/Home.jsx
+++ src/views/admin/pages/Order/screens/Home/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import ProfileBar from '../../../../components/ProfileBar/ProfileBar'
 import profilePic from '../../../../assets/images/Ellipse 1.png'
 import OrderTable from '../../components/OrdersTable/OrderTable'
+import StatusButton from '../../../../components/StatusButton/StatusButton'
+import axios from 'axios'
+import Cookies from 'js-cookie'
 const Home = () => {
+    const [data, setData] = useState('');
+    const baseUrl = process.env.REACT_APP_BASE_URL
+    const token = Cookies.get("token");
+    const getAllOrders = async () => {
+        await axios.get(baseUrl + "/api/allOrders", {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }).then(response => {
+            setData(response.data.data)
+        })
+    }
+    useEffect(() => {
+        getAllOrders()
+    }, [])
+    console.log(data);
     return (
         <>
             <div className="order_homeContainer">
@@ -15,15 +34,26 @@ const Home = () => {
                 </div>
                 <div className="order_TableContainer">
                     <OrderTable
-                        tableHeads={["Id", "name", "skills", "experience", "IQ"]}
-                        array={[
-                            { id: 1, name: "arshman", skills: "web", experience: "2", iq: "100", height: "5.7", color: "light brown", cast: "fighters" },
-                            { id: 1, name: "arshman", skills: "web", experience: "2", iq: "100", height: "5.7", color: "light brown", cast: "fighters" },
-                            { id: 1, name: "arshman", skills: "web", experience: "2", iq: "100", height: "5.7", color: "light brown", cast: "fighters" },
-                            { id: 1, name: "arshman", skills: "web", experience: "2", iq: "100", height: "5.7", color: "light brown", cast: "fighters" },
-                            { id: 1, name: "arshman", skills: "web", experience: "2", iq: "100", height: "5.7", color: "light brown", cast: "fighters" },
-                        ]}
-                        keysToDisplay={["id", "name", "skills", "experience", "iq"]}
+                        tableHeads={["Order ID", "Phone No", "All Dates", "Delivery Dates", "Amount", "Status"]}
+                        array={data}
+                        keysToDisplay={["id", "phoneNumber", "createdAt", "dateOfDelivery", "totalPrice", "status"]}
+                        ConditionalModifiedColumn={
+                            () => {
+                                return {
+                                    status: (value) => {
+                                        if (value = "Completed") {
+                                            return <StatusButton status="Completed" />
+                                        }
+                                        if (value = "Cancel") {
+                                            return <StatusButton status="Cancel" />
+                                        }
+                                        if (value = "Processing") {
+                                            return <StatusButton status="Processing" />
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     />
                 </div>
             </div>
