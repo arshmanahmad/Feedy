915857a2558aad7d467742487526fcd4c0079f05
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-04-08 08:32:37 -0700

    update

--- src/views/admin/pages/Order/components/OrdersTable/Pagination.jsx
+++ src/views/admin/pages/Order/components/OrdersTable/Pagination.jsx
@@ -44,7 +44,9 @@ const Pagination = ({
             setActiveArrow("active_arrow")
             setPreviousActiveArrow("table_arrow")
         }
-        //comment
+
+
+
     }, [group])
     console.log(currentPage, pairNumbers.length - 1);
     const handleGroup = (groupNumber) => {
